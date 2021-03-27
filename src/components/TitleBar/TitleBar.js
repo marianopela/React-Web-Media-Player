5353366259453adce4abdaf2148d52a95128c3c0
Amin Mahboubi <aminmahboobi@gmail.com>
2021-03-27 23:31:06 +0100

    export TitleBar

--- src/components/TitleBar/TitleBar.js
+++ src/components/TitleBar/TitleBar.js
@@ -1,41 +1,52 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './TitleBar.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./TitleBar.css";
 
-class TitleBar extends Component {
-    handleClick = (e) => {
-        e.stopPropagation();
+export class TitleBar extends Component {
+  handleClick = (e) => {
+    e.stopPropagation();
+  };
+
+  render = () => {
+    let title;
+    if (this.props.link) {
+      title = (
+        <a
+          className="wmp-title light-grey-to-white"
+          href={this.props.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {this.props.title}
+        </a>
+      );
+    } else {
+      title = (
+        <span className="wmp-title light-grey-to-white">
+          {this.props.title}
+        </span>
+      );
     }
-
-    render = () => {
-        let title
-        if (this.props.link) {
-            title = <a className="wmp-title light-grey-to-white" href={this.props.link} target="_blank" rel="noopener noreferrer">{this.props.title}</a>
-        } else {
-            title = <span className="wmp-title light-grey-to-white">{this.props.title}</span>;
-
-        }
-        /*
+    /*
         <div class="salefi-player-title-container">
             <a class="salefi-player-title light-grey-to-white" href="#" target="_blank">Product name - Store name</a>
             <div class="salefi-player-top-shading"></div>
             </div>
         */
-        return (
-            <div className="wmp-title-container" onClick={this.handleClick}>
-                {title}
-                <div className="wmp-top-shading"></div>
-            </div>
-            
-        );
-    }
+    return (
+      <div className="wmp-title-container" onClick={this.handleClick}>
+        {title}
+        <div className="wmp-top-shading"></div>
+      </div>
+    );
+  };
 }
 
 const mapStateToProps = (state) => {
-    return {
-        title: state.title,
-        link: state.link
-    };
+  return {
+    title: state.title,
+    link: state.link,
+  };
 };
 
 export default connect(mapStateToProps)(TitleBar);
